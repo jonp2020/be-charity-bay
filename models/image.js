7ad4e0ab48f10c05b480fbed769a3cc60b3957f6
JFParrott <jfparrott@protonmail.com>
2020-11-10 11:33:59 +0000

    remove console logs

--- models/image.js
+++ models/image.js
@@ -12,7 +12,6 @@ AWS.config.update({
 const s3 = new AWS.S3();
 
 const uploadFile = async (buffer, name, type) => {
-  console.log(process.env.AWS_ACCESS_KEY_ID, process.env.S3_BUCKET);
   const params = {
     ACL: 'public-read',
     Body: buffer,
--- models/items.js
+++ models/items.js
@@ -1,94 +1,94 @@
-const connection = require("../db/connection");
-const { checkExists } = require("./utils");
+const connection = require('../db/connection');
+const { checkExists } = require('./utils');
 
 exports.selectItems = async (status, buyer, category, p = 1, limit = 10) => {
   if (p != Number(p) || limit != Number(limit)) {
-    return Promise.reject({ status: 400, msg: "Bad Request" });
+    return Promise.reject({ status: 400, msg: 'Bad Request' });
   }
   if (p < 1) p = 1;
   const offsetAmount = (p - 1) * limit;
-  const items = await connection("items")
-    .select("*")
+  const items = await connection('items')
+    .select('*')
     .limit(limit)
     .offset(offsetAmount)
     .modify((query) => {
-      if (status) query.where("status", status);
-      if (category) query.where("category", category);
-      if (buyer) query.where("buyer", buyer);
+      if (status) query.where('status', status);
+      if (category) query.where('category', category);
+      if (buyer) query.where('buyer', buyer);
     });
-  const itemCount = await connection("items")
-    .select("*")
+  const itemCount = await connection('items')
+    .select('*')
     .modify((query) => {
-      if (status) query.where("status", status);
-      if (category) query.where("category", category);
-      if (buyer) query.where("buyer", buyer);
+      if (status) query.where('status', status);
+      if (category) query.where('category', category);
+      if (buyer) query.where('buyer', buyer);
     });
 
   if (!items.length) {
     await Promise.all([
-      checkExists("categories", "slug", category),
-      checkExists("users", "username", buyer),
+      checkExists('categories', 'slug', category),
+      checkExists('users', 'username', buyer),
     ]);
   }
   if (status) {
     if (
-      status === "available" ||
-      status === "reserved" ||
-      status === "purchased"
+      status === 'available' ||
+      status === 'reserved' ||
+      status === 'purchased'
     ) {
       return { items, itemCount: itemCount.length };
     } else {
-      return Promise.reject({ status: 400, msg: "Bad Request" });
+      return Promise.reject({ status: 400, msg: 'Bad Request' });
     }
   }
   return { items, itemCount: itemCount.length };
 };
 
 exports.insertItem = async (itemBody) => {
-  const [item] = await connection("items").insert(itemBody, "*");
+  const [item] = await connection('items').insert(itemBody, '*');
   return item;
 };
 
 exports.selectItemById = async (item_id) => {
-  const item = await connection("items")
-    .select("*")
+  const item = await connection('items')
+    .select('*')
     .first()
     .where({ item_id: item_id });
   if (!item) {
-    return Promise.reject({ status: 404, msg: "Item Not Found" });
+    return Promise.reject({ status: 404, msg: 'Item Not Found' });
   }
   return item;
 };
 
 exports.updateItemById = async (status, buyer, item_id) => {
-  if (status === "purchased") {
-    const [item] = await connection("items")
+  if (status === 'purchased') {
+    const [item] = await connection('items')
       .where({ item_id: item_id })
       .update({ status })
-      .returning("*");
+      .returning('*');
     if (!item) {
-      return Promise.reject({ status: 404, msg: "Item Not Found" });
+      return Promise.reject({ status: 404, msg: 'Item Not Found' });
     }
     return item;
   }
-  if (status === "reserved" && typeof buyer === "string") {
-    const [item] = await connection("items")
+  if (status === 'reserved' && typeof buyer === 'string') {
+    const [item] = await connection('items')
       .where({ item_id: item_id })
       .update({ status, buyer })
-      .returning("*");
+      .returning('*');
     if (!item) {
-      return Promise.reject({ status: 404, msg: "Item Not Found" });
+      return Promise.reject({ status: 404, msg: 'Item Not Found' });
     }
     return item;
   } else {
-    return Promise.reject({ status: 400, msg: "Bad Request" });
+    return Promise.reject({ status: 400, msg: 'Bad Request' });
   }
 };
 
 exports.delItemById = async (item_id) => {
-  const deletedRows = await connection("items")
+  const deletedRows = await connection('items')
     .where({ item_id: item_id })
     .del();
   if (deletedRows === 1) return;
-  else return Promise.reject({ status: 404, msg: "Item Not Found" });
+  else return Promise.reject({ status: 404, msg: 'Item Not Found' });
 };
